2ac5e277546efc4bcf46d9648b8e509e367a68ae
Avinash <imavinashmasih@gmail.com>
2019-10-17 19:24:41 -0600

    Auto stash before merge of "master" and "origin/master"

--- OculusT/Assets/Scripts/HandPoseGuide.cs
+++ OculusT/Assets/Scripts/HandPoseGuide.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandPoseGuide : MonoBehaviour
+public class HandPoseGuide 
+    : MonoBehaviour
 {
+    private OVRInput.Controller m_controller;
     protected bool m_VRhandDetected = false;
     public Color greencolor;
     public Color whitecolor;
 
     private void OnTriggerEnter(Collider other)
     {
-        m_VRhandDetected = true;
-        transform.GetComponent<Renderer>().material.color = greencolor;
+        if(!OVRInput.Get(OVRInput.NearTouch.PrimaryIndexTrigger, m_controller) == true)
+        {
+            m_VRhandDetected = true;
+            transform.GetComponent<Renderer>().material.color = greencolor;
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
@@ -19,5 +25,5 @@ public class HandPoseGuide : MonoBehaviour
         m_VRhandDetected = false;
         transform.GetComponent<Renderer>().material.color = whitecolor;
     }
-
+    //m_isGivingThumbsUp = !OVRInput.Get(OVRInput.NearTouch.PrimaryThumbButtons, m_controller);
 }

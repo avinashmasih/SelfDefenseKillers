b53f011827493d1856492ca310e416d2c2dd7dd3
Erika <sunny_girl16@live.com>
2019-10-22 20:51:51 -0600

    testing object movement with trail

---
+++ OculusT/Assets/Scripts/TrajectoryDataContent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrajectoryDataContent : MonoBehaviour
+{
+    private int _lineCount;
+
+    private void Update()
+    {
+
+    }
+
+    public void OnLaunchEvent(string data)
+    {
+        if (!string.IsNullOrEmpty(data))
+        {
+            Text text = GetComponent<Text>();
+
+            if (text == null)
+                return;
+
+            if (_lineCount == 0)
+            {
+                text.text = string.Empty;
+            }
+
+            text.text += data + Environment.NewLine;
+
+            _lineCount++;
+        }
+    }
+}

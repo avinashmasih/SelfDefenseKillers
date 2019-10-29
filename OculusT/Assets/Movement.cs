d96411466a1d54089b14351f079fb17fbb82b713
LuciVatsa <saaranshvatsa@yahoo.in>
2019-10-29 09:54:27 -0600

    Ghost

--- OculusT/Assets/Movement.cs
+++ OculusT/Assets/Movement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public GameObject[] waypoints;
+    int current = 0;
+    public float speed;
+    float WPradius = 1;
+
     public Vector3 initial;
     public Vector3 final;
 
@@ -16,6 +21,14 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
     }
 }

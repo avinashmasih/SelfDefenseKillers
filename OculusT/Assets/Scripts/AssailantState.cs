eb54ad4530f11dd16c7b1be845abe2d0936d2e3d
Avinash <imavinashmasih@gmail.com>
2019-10-29 11:34:26 -0600

    Added animation states and controller

---
+++ OculusT/Assets/Scripts/AssailantState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssailantState : MonoBehaviour
+{
+    public Animator anim;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("startAttack");
+        }
+        if(Input.GetKey(KeyCode.X))
+        {
+            anim.SetBool("gotPoked", true);
+        }
+    }
+}

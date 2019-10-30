228007aa362ac51aac5cc1ccfb3ed54cd9a085ea
Avinash <imavinashmasih@gmail.com>
2019-10-29 21:14:59 -0600

    Added all the states

--- OculusT/Assets/Scripts/AssailantState.cs
+++ OculusT/Assets/Scripts/AssailantState.cs
@@ -5,16 +5,43 @@ using UnityEngine;
 public class AssailantState : MonoBehaviour
 {
     public Animator anim;
+    public bool reset = false;
 
     private void Update()
     {
         if (OVRInput.Get(OVRInput.Button.One))
         {
-            anim.SetTrigger("startAttack");
+            anim.SetTrigger("pokeApproach");
         }
-        if(OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            anim.SetBool("gotPoked", true);
+            anim.SetTrigger("groinApproach");
         }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            anim.SetTrigger("punchApproach");
+        }
+        //Eye Poke
+        //anim.SetBool("gotPoked", true);
+        //groin punch
+        //anim.SetBool("groinHit", true);
+        //Block
+        //anim.SetBool("gotBlock", true);
+        //Reset to idle
+        //anim.SetBool("reset",true);
+        //Reset()
+    }
+
+    private void Reset()
+    {
+        //Clearing out
+        //Eye Poke
+        //anim.SetBool("gotPoked", false);
+        //groin punch
+        //anim.SetBool("groinHit", false);
+        //Block
+        //anim.SetBool("gotBlock", false);
+        //Reset to idle
+        //anim.SetBool("reset",false);
     }
 }
